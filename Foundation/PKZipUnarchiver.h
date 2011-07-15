876416128b97a683ccf9117c00274fc101ded513
Pavel Kunc <pavel.kunc@gmail.com>
2011-07-15 15:27:26 +0100

    Adding PKZipUnarchiver for unzip with progress delegate.

---
+++ Foundation/PKZipUnarchiver.h
@@ -0,0 +1,40 @@
+//
+//  PKZipUnarchiver.h
+//  PKToolbox
+//
+//  Created by Pavel Kunc on 29/04/2011.
+//  Copyright (C) 2011 by Pavel Kunc, http://pavelkunc.cz
+
+#import <Foundation/Foundation.h>
+
+#import "ZipFile.h"
+#import "ZipFileInfo.h"
+#import "ZipReadStream.h"
+
+
+#ifndef
+#define PK_ZIPUNARCHIVER_BUFFER_SIZE (1024 * 32)
+#endif
+
+@protocol PKZipUnarchiverDelegate <NSObject>
+@optional
+-(void)didStartUncompressingZip;
+-(void)didUncompressBytes:(NSUInteger)bytes total:(NSUInteger)total;
+-(void)didFinishUncompressingZip;
+@end
+
+
+@interface PKZipUnarchiver : NSObject {
+@private
+    NSString                    *_zipPath;
+    id<PKZipUnarchiverDelegate>  _delegate;
+}
+
+@property (nonatomic, assign, readwrite) id<PKZipUnarchiverDelegate> delegate;
+
+- (id)initWithZipAtPath:(NSString *)aPath;
+- (id)initWithZipAtPath:(NSString *)aPath delegate:(id<PKZipUnarchiverDelegate>)aDelegate;
+- (BOOL)unzipTo:(NSString *)aDestination error:(NSError **)outError;
+
+@end
+
---
+++ Foundation/PKZipUnarchiver.m
@@ -0,0 +1,105 @@
+//
+//  PKZipUnarchiver.m
+//  PKToolbox
+//
+//  Created by Pavel Kunc on 29/04/2011.
+//  Copyright (C) 2011 by Pavel Kunc, http://pavelkunc.cz
+
+
+#import "PKZipUnarchiver.h"
+
+@interface PKZipUnarchiver ()
+@property (nonatomic, assign, readonly) NSUInteger  totalUncompressedSize;
+@property (nonatomic, copy, readonly)   NSString   *zipPath;
+@end
+
+
+@implementation PKZipUnarchiver
+
+@synthesize delegate              = _delegate;
+@synthesize zipPath               = _zipPath;
+
+
+#pragma mark - Initialization/dealloc
+
+- (void)dealloc {
+    [_zipPath release];
+    [super dealloc];
+}
+
+- (id)initWithZipAtPath:(NSString *)aPath delegate:(id<PKZipUnarchiverDelegate>)aDelegate {
+    if (self != nil) {
+        _zipPath = [aPath copy];
+        _delegate = aDelegate;
+    }
+    return self;
+}
+
+- (id)initWithZipAtPath:(NSString *)aPath {
+    return [self initWithZipAtPath:aPath delegate:nil];
+}
+
+
+#pragma mark - Unzip methods
+
+- (BOOL)unzipTo:(NSString *)aDestination error:(NSError **)outError {
+    ZipFile *zipFile = [[ZipFile alloc] initWithFileName:_zipPath mode:ZipFileModeUnzip];
+
+    if (![zipFile goToFirstFileInZip:&outError]) {
+        return NO;
+    }
+
+    NSUInteger totalUncompressedSize = 0;
+    if ([self.delegate respondsToSelector:@selector(didUncompressBytes:total:)]) {
+        NSArray *zipFileInformation = [zipFile containedFiles];
+        ZipFileInfo *fileInfo;
+        for (fileInfo in zipFileInformation) {
+            totalUncompressedSize += fileInfo.length;
+        }
+    }
+
+    BOOL isDirectory;
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSString *currentFilePath;
+    NSUInteger currentFileIndex = 0;
+    do {
+        ZipReadStream *file = [zipFile readCurrentFileInZip:&outError];
+        ZipFileInfo *fileInfo = [zipFileInformation objectAtIndex:currentFileIndex];
+        isDirectory = [fileInfo.name hasSuffix:@"/"];
+        currentFilePath = [aDestination stringByAppendingPathComponent:fileInfo.name];
+
+        if (isDirectory) {
+            [fileManager createDirectoryAtPath:currentFilePath
+                   withIntermediateDirectories:YES
+                                    attributes:nil
+                                         error:&outError];
+        } else {
+            [fileManager createFileAtPath:currentFilePath
+                                 contents:nil
+                               attributes:nil];
+
+            NSUInteger bytesRead = 0;
+            NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:currentFilePath];
+            NSMutableData *buffer = [[NSMutableData alloc] initWithLength:BUFFER_SIZE];
+            while ((bytesRead = [file readDataWithBuffer:buffer error:&outError]) != 0) {
+                [fileHandle writeData:buffer];
+                if ([self.delegate respondsToSelector:@selector(didUncompressBytes:total:)]) {
+                    [self.delegate didUncompressBytes:bytesRead total:totalUncompressedSize];
+                }
+            }
+            [buffer release]; buffer = nil;
+            [fileHandle closeFile];
+        }
+        [file finishedReadingWithError:&outError];
+        currentFileIndex++;
+    } while([zipFile goToNextFileInZip:&outError]);
+
+    [fileManager release];
+    [zipFile close];
+    [zipFile release];
+
+    return YES;
+}
+
+@end
+
