ca1c4482ef2963d86c19247f64a87c588a085a35
Pavel Kunc <pavel.kunc@gmail.com>
2012-05-22 16:55:54 +0100

    Add Category to generate UUID strings.

---
+++ PKToolbox/Sources/Foundation/NSString+PKUUID.h
@@ -0,0 +1,15 @@
+//
+//  NSString+PKUUID.h
+//  PKToolbox
+//
+//  Created by Pavel Kunc on 21/05/2012.
+//  Copyright (c) 2012 Fry-it, Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PKUUID)
+
++ (NSString *)pk_UUID;
+
+@end
